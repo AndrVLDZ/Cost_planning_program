1437b28775bf9e25be5ba5ce93148c3678376e1d
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2021-12-15 17:41:12 +0300

    feat: data addition to SQLite

--- calculation.py
+++ calculation.py
@@ -1,7 +1,9 @@
 import sqlite3
 from sqlite3 import Error
-import datetime
+import traceback
+# import datetime
 from dataclasses import dataclass
+from typing import Counter
 
 @dataclass(frozen=True)
 class TermColors:
@@ -31,7 +33,7 @@ def db_check():
       with sqlite3.connect('Fare.db') as db:
             c = db.cursor()   
             query_1 = ''' 
-            CREATE TABLE IF NOT EXISTS trips (
+            CREATE TABLE IF NOT EXISTS costs (
                   id INTEGER PRIMARY KEY,
                   type TEXT,
                   price INTEGER,
@@ -40,11 +42,11 @@ def db_check():
             c.execute(query_1)
             print("Подключен к SQLite")
 
-def db_insert():
+def db_insert_default_values():
       with sqlite3.connect('Fare.db') as db:
             c = db.cursor()
             query_2 = '''
-            INSERT OR REPLACE INTO trips(id, type, price, number)
+            INSERT OR REPLACE INTO costs(id, type, price, number)
             VALUES
             (1,'Метро',41,44),
             (2,'Маршрутка',40,22)
@@ -56,49 +58,92 @@ def db_insert():
 def db_print():
       with sqlite3.connect('Fare.db') as db:
             c = db.cursor()
-            c.execute("SELECT * FROM trips")
+            c.execute("SELECT * FROM costs")
             records = c.fetchall()
             for row in records:
                   print(row)
                   
-
-def db_read_data(row, column, table="trips"):
+def db_row_cnt(table="costs") -> int:
       with sqlite3.connect('Fare.db') as db:
-          sqlite_connection = sqlite3.connect('Fare.db')
-          c = sqlite_connection.cursor()
+          c = db.cursor()
+          sqlite_select_query = f"SELECT Count(*) from {table}"
+          c.execute(sqlite_select_query)
+          cnt = c.fetchone()
+      #     print(cnt[0])
+          return cnt[0]
+
+db_row_cnt()
+
+def db_read_data(row: int, column: int, table="costs") -> str:
+      with sqlite3.connect('Fare.db') as db:
+          c = db.cursor()
           sqlite_select_query = f"SELECT * from {table}"
           c.execute(sqlite_select_query)
           records = c.fetchall()
           value = records[row][column]
-          return value 
+          return value
+
+def db_edit_data():
+      pass 
+
+# def db_insert_data(id: int, type: str, price: float, number: int, values: list, table="costs"):
+#       with sqlite3.connect('Fare.db') as db:
+#           c = db.cursor()
+#           for item in values:
+#               c.execute(f'''
+#                     INSERT OR REPLACE INTO {table}({id}, {type}, {price}, {number})
+#                     VALUES
+#                     {item}
+#                     ''') 
+#       #     c.execute(sqlite_insert_query)
+#           db.commit
+#           print(f"{TermColors.GREEN}записей добавлено: {c.rowcount}{TermColors.ENDC}")
+
+def db_insert_data(values: list, table="costs") -> None:
+      with sqlite3.connect('Fare.db') as db:
+          c = db.cursor()
+          row_cnt: int = 0
+          for item in values:
+              c.execute(f'''
+                    INSERT OR REPLACE INTO {table}(id, type, price, number)
+                    VALUES
+                    {item}
+                    ''')
+              row_cnt += 1 
+          db.commit
+          print(f"{TermColors.GREEN}Записей добавлено: {row_cnt}{TermColors.ENDC}")
 
 def calculation():
       with sqlite3.connect('Fare.db') as db:
             c = db.cursor()
-            c.execute("SELECT price, number FROM trips")
+            c.execute("SELECT price, number FROM costs")
             records = c.fetchall()
             res = 0
             for row in records:
                   res += row[0] * row[1]
             return res
 
-def trips_data_rich():
+def costs_data_rich():
       from rich.console import Console
       from rich.table import Table
       table = Table(title='Таблица расходов')
       table.add_column('Тип')
       table.add_column('Цена')
       table.add_column('Кол-во')
-      table.add_row(str(db_read_data(0,1)), str(db_read_data(0,2)), str(db_read_data(0,3)))
-      table.add_row(str(db_read_data(1,1)), str(db_read_data(1,2)), str(db_read_data(1,3)))
-      console = Console()
+      rows = db_row_cnt()
+      for row in range(rows):
+            table.add_row(str(db_read_data(row,1)), str(db_read_data(row,2)), str(db_read_data(row,3)))
+
+            # table.add_row(str(db_read_data(0,1)), str(db_read_data(0,2)), str(db_read_data(0,3)))
+            # table.add_row(str(db_read_data(1,1)), str(db_read_data(1,2)), str(db_read_data(1,3)))
+            console = Console()
       console.print(table)
 
 def dialog():
       try:
             option = int(input('Выберите пункт меню: '))
             if option == 1:
-                  trips_data_rich()
+                  costs_data_rich()
                   menu()
                   dialog() 
             elif option == 2:
@@ -117,13 +162,15 @@ def dialog():
                   menu()
                   dialog() 
       except:
-            print('Ошибка ввода!')
+            print('Ошибка ввода:\n', traceback.format_exc())
             menu()
             dialog()
 
 if __name__ == '__main__':
       db_check()
-      db_insert()
+      db_insert_default_values()
+      data = [(1,'Метро',41,44), (2,'Маршрутка',40,22)]
+      db_insert_data(data)
       menu()
       dialog()
       
